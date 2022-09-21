2b39f8c0632f4b0ffc2b81a2f4dded3db225f191
= <=>
2022-09-21 21:45:51 +0700

    foe

--- src/main/java/com/codegym/cms/repository/IProvinceRepository.java
+++ src/main/java/com/codegym/cms/repository/IProvinceRepository.java
@@ -1,9 +1,10 @@
 package com.codegym.cms.repository;
 
 import com.codegym.cms.model.Province;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IProvinceRepository extends PagingAndSortingRepository<Province, Long> {
+public interface IProvinceRepository extends CrudRepository<Province, Long> {
 }
